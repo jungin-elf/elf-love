f3fbe1f58a307c48fde39d311253bdb443a0480d
elf-love <siga0411@gmail.com>
2025-07-14 10:14:20 +0900

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>🏌️‍♀️ 엘프사랑 골프동호회 홈페이지</h1>
+      <p>홈페이지가 곧 오픈됩니다. 기대해주세요!</p>
+    </div>
+  );
+}
+
+export default App;
