0a56a319a471a4ffd3d807042097e1a38f7f244b
elf-love <siga0411@gmail.com>
2025-07-14 22:52:56 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Signup from "./pages/Signup"; // 추가
+import Home from "./pages/Home";
+import Signup from "./pages/Signup"; // 회원가입 페이지 import
 
 function App() {
   return (
     <Router>
       <Header />
-      <main style={{ padding:
+      <main style={{ padding: "20px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} /> {/* 회원가입 라우트 추가 */}
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
