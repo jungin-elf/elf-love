20d165078d659fabd0d8414a8b818bdf2de5c7d5
elf-love <siga0411@gmail.com>
2025-07-14 23:12:55 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";  // 로그인 컴포넌트 위치에 맞게 경로 조정
-import Home from "./pages/Home";    // 메인 페이지 컴포넌트도 만들어야 함
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
