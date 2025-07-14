a656926aaca31c148f27ec4c7da5804d95f66ab0
elf-love <siga0411@gmail.com>
2025-07-14 23:12:36 +0900

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>엘프사랑 골프동호회 메인페이지</h1>
+      <p>여기에 대회별 랭킹, 포인트 랭킹, 차기 대회 일정 등이 표시될 예정입니다.</p>
+    </div>
+  );
+};
+
+export default Home;
